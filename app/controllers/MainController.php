340bb5a06cf806a1ea53e06ba12f3fd295e3666e
Antonio Diaz <antoniodiazduran@gmail.com>
2023-09-30 14:12:08 -0400

    MainController route fixed

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -2,10 +2,10 @@
 
 class MainController extends Controller {
 
-    public function main_index(){
-        $this->f3->set('page_head','Capture your data...');
-        $this->f3->set('view','main.htm');
-	echo \Template::instance()->render('welcome.htm');
+    public function index(){
+        $this->f3->set('layout','layout.htm');
+        $this->f3->set('content','main.htm');
+	echo \Template::instance()->render($this->f3->get('layout'));
     }
 
 } // main class
